bc5654379892c76c243bab8a38b1054311febcb1
Emre Hasegeli <hasegeli@tart.com.tr>
2013-03-25 15:22:16 +0200

    hget function added to get value of a field from a Redis hash as an array.

--- redis_fdw--1.0.sql
+++ redis_fdw--1.0.sql
@@ -27,3 +27,22 @@ LANGUAGE C STRICT;
 CREATE FOREIGN DATA WRAPPER redis_fdw
   HANDLER redis_fdw_handler
   VALIDATOR redis_fdw_validator;
+
+CREATE FUNCTION hget(hash text[], field text) RETURNS text AS $$
+DECLARE
+    value text;
+    found boolean := FALSE;
+BEGIN
+   FOREACH value IN ARRAY hash
+   LOOP
+       IF found THEN
+           RETURN value;
+       END IF;
+       IF value = field THEN
+           found := TRUE;
+       END IF;
+   END LOOP;
+   RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
